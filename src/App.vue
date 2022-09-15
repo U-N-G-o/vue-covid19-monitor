4172d6cbb0dcba81fab4b3b6c80d04dd1573bb97
U-N-G-o <913715359@qq.com>
2022-09-15 19:26:05 +0800

    feat: 完成疫情地图绘制

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ChinaMap from './components/ChinaMap.vue'
+
+</script>
+
+<template>
+  <div class="w-screen h-screen flex flex-col">
+    <div class="h-[80px] flex justify-center items-center">
+      <span class="text-4xl font-bold">全国新冠肺炎疫情数据地图</span>
+    </div>
+    <div class="flex-1">
+      <ChinaMap />
+    </div>
+  </div>
+</template>
---
+++ src/components/ChinaMap.vue
@@ -0,0 +1,113 @@
+<script setup>
+import chinaMapJson from '../assets/json/china.json'
+import listJson from '../assets/json/list.json'
+import { ref, getCurrentInstance, onMounted } from 'vue'
+
+const chinaMap = ref()
+const {proxy: { $echarts }} = getCurrentInstance()
+
+const list = JSON.parse(JSON.stringify(listJson))
+
+const chinaData = list.data.areaTree.find(item => item.name === '中国')
+
+/** 格式化接口中的 name 数据 */
+const formatName = (name) => {
+  switch(name) {
+    case '上海':
+    case '北京':
+    case '天津':
+    case '重庆':
+      return name + '市';
+    case '西藏':
+    case '内蒙古':
+      return name + '自治区';
+    case '新疆':
+      return name + '维吾尔自治区';
+    case '广西':
+      return name + '壮族自治区';
+    case '宁夏':
+      return name + '回族自治区';
+    case '香港':
+    case '澳门':
+      return name + '特别行政区'
+    default:
+      return name + '省'
+  }
+}
+
+/** 各地区的确诊人数 */
+const confirmData = chinaData.children.map(item => {
+  const { total: { confirm, heal, dead } } = item
+  return {
+    name: formatName(item.name),
+    /** 现有确诊 = 确诊人数 - 治愈人数 - 死亡人数 */
+    value: confirm - heal - dead
+  }
+})
+
+onMounted(() => {
+  drawMap()
+});
+
+function drawMap() {
+  const myChart = $echarts.init(chinaMap.value)
+  $echarts.registerMap('china', chinaMapJson) //注册可用的地图
+  const option = {
+    geo: {
+      map: 'china',
+      roam: true, //是否允许缩放，拖拽
+      zoom: 1.5, //初始化大小
+      //缩放大小限制
+      scaleLimit: {
+        min: 1, //最小
+        max: 2, //最大
+      },
+      //设置中心点
+      center: [108, 35],
+      itemStyle: {
+        areaColor: '#1af9e5',
+        color: 'red',
+        borderColor: '#232652',
+        borderWidth: 1,
+      },
+      //高亮状态
+      emphasis: {
+        itemStyle: {
+          areaColor: '#374ba4',
+          color: '#fff',
+        },
+      },
+    },
+    tooltip: {
+      trigger: 'item',
+    },
+    visualMap: {
+      left: 'right',
+      type: 'piecewise',
+      pieces: [
+        {min: 10000, color: '#a50026' },
+        {min: 1000, max: 9999, color: '#d73027'},
+        {min: 500, max: 999, color: '#f46d43'},
+        {min: 100, max: 499, color: '#fee090'},
+        {min: 10, max: 99, color: '#ffffbf'},
+        {min: 0, max: 9, color: '#abd9e9'}
+      ],
+    },
+    //配置属性
+    series: [
+      {
+        name: '确诊病例',
+        type: 'map',
+        map: 'china',
+        geoIndex: 0,
+        data: confirmData,
+      }
+    ],
+  }
+  myChart.setOption(option)
+}
+</script>
+
+<template>
+  <div ref="chinaMap" class="h-full"></div>
+</template>
